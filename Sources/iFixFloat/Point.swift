064c3b3be706edc66df71eef180a8765ab738e4d
Nail Sharipov <nailxsharipov@gmail.com>
2024-04-13 19:36:47 +0300

    math api for Point

--- Sources/iFixFloat/Point.swift
+++ Sources/iFixFloat/Point.swift
@@ -12,7 +12,45 @@ public typealias Point = SIMD2<Int32>
 public extension Point {
     
     @inline(__always)
+    var sqrLength: Int64 {
+        let x = Int64(x)
+        let y = Int64(y)
+        return x * x + y * y
+    }
+
+    @inline(__always)
     init(_ p: FixVec) {
         self = .init(x: Int32(p.x), y: Int32(p.y))
+    }
+    
+    @inline(__always)
+    func crossProduct(_ v: Point) -> Int64 { // cross product (sin)
+        let a = Int64(x) * Int64(v.y)
+        let b = Int64(y) * Int64(v.x)
+
+        return a - b
+    }
+    
+    @inline(__always)
+    func dotProduct(_ v: Point) -> Int64 { // dot product (cos)
+        let xx = Int64(x) * Int64(v.x)
+        let yy = Int64(y) * Int64(v.y)
+        return xx + yy
+    }
+    
+    
+    @inline(__always)
+    func subtract(_ other: Point) -> FixVec {
+        let x = Int64(self.x) - Int64(other.x)
+        let y = Int64(self.y) - Int64(other.y)
+        return FixVec(x, y)
+    }
+    
+    
+    @inline(__always)
+    func sqrDistance(_ v: Point) -> Int64 {
+        let x = Int64(x) - Int64(v.x)
+        let y = Int64(y) - Int64(v.y)
+        return x * x + y * y
     }
 }
--- Sources/iFixFloat/Triangle.swift
+++ Sources/iFixFloat/Triangle.swift
@@ -43,15 +43,20 @@ public struct Triangle {
     }
     
     @inline(__always)
+    public static func isLine(p0: FixVec, p1: FixVec, p2: FixVec) -> Bool {
+        unsafeAreaTwo(p0: p0, p1: p1, p2: p2) == 0
+    }
+    
+    @inline(__always)
     public static func clockDirection(p0: FixVec, p1: FixVec, p2: FixVec) -> Int64 {
         unsafeAreaTwo(p0: p0, p1: p1, p2: p2).signum()
     }
     
     @inline(__always)
-    public static func isContain(p: FixVec, p0: FixVec, p1: FixVec, p2: FixVec) -> Bool {
-        let q0 = (p - p1).crossProduct(p0 - p1)
-        let q1 = (p - p2).crossProduct(p1 - p2)
-        let q2 = (p - p0).crossProduct(p2 - p0)
+    public static func isContain(p: Point, p0: Point, p1: Point, p2: Point) -> Bool {
+        let q0 = p.subtract(p1).crossProduct(p0.subtract(p1))
+        let q1 = p.subtract(p2).crossProduct(p1.subtract(p2))
+        let q2 = p.subtract(p0).crossProduct(p2.subtract(p0))
         
         let has_neg = q0 < 0 || q1 < 0 || q2 < 0
         let has_pos = q0 > 0 || q1 > 0 || q2 > 0
@@ -69,5 +74,35 @@ public struct Triangle {
         let has_pos = q0 >= 0 || q1 >= 0 || q2 >= 0
         
         return has_neg && has_pos
+    }
+
+    @inline(__always)
+    public static func unsafeAreaTwo(p0: Point, p1: Point, p2: Point) -> Int64 {
+        p1.subtract(p0).crossProduct(p1.subtract(p2))
+    }
+    
+    @inline(__always)
+    public static func isClockwise(p0: Point, p1: Point, p2: Point) -> Bool {
+        unsafeAreaTwo(p0: p0, p1: p1, p2: p2) > 0
+    }
+    
+    @inline(__always)
+    public static func isCW_or_Line(p0: Point, p1: Point, p2: Point) -> Bool {
+        unsafeAreaTwo(p0: p0, p1: p1, p2: p2) >= 0
+    }
+    
+    @inline(__always)
+    public static func isNotLine(p0: Point, p1: Point, p2: Point) -> Bool {
+        unsafeAreaTwo(p0: p0, p1: p1, p2: p2) != 0
+    }
+    
+    @inline(__always)
+    public static func isLine(p0: Point, p1: Point, p2: Point) -> Bool {
+        unsafeAreaTwo(p0: p0, p1: p1, p2: p2) == 0
+    }
+    
+    @inline(__always)
+    public static func clockDirection(p0: Point, p1: Point, p2: Point) -> Int64 {
+        unsafeAreaTwo(p0: p0, p1: p1, p2: p2).signum()
     }
 }
